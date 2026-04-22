59ecd005c9d7dba522e8f960b8a272d8cef7a9dc
GiJeongCho <wzxcv123@naver.com>
2026-04-22 14:45:12 +0900

    뭐더라

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pps-nipa/NIQ/fish/side/g/US_Stock_searcher
 /home/pps-nipa/anaconda3/bin/gunicorn \
-  --bind 0.0.0.0:5065 \
+  --bind 0.0.0.0:4999 \
   --workers 2 \
   --threads 4 \
   --timeout 300 \
